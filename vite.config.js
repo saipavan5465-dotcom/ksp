44c3534b4b9c1906c3dbd9dc14c693fa329c99d8
saipavan5465-dotcom <saipavan5465@gmail.com>
2026-02-02 15:57:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Refer to this main Documentation - https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ksp/'
 })
